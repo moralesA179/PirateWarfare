a96d7907cefc69cd2dd2e6d7668fb3c09ec73691
EdgarQuinones <edgarq1215@gmail.com>
2026-04-05 21:23:30 -0400

    Animations added
    - Animations based on the ship health, making it look more destroyed the less health it has
    - Fire shows when cannons are shot for a more feel when shooting

--- PirateWarfare/Assets/_Project/Scripts/PlayerData.cs
+++ PirateWarfare/Assets/_Project/Scripts/PlayerData.cs
@@ -2,7 +2,53 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
-    public static int health = 100, score = 0;
+    public enum HealthState 
+    { 
+        Full = 0, 
+        Half = 1, 
+        Low = 2
+    }
+
+    public static int maxHealth = 100, currentHealth = maxHealth, score = 0;
     public static float speedMult = 1f;
     //public static ItemList[] <-- Future(We need to keep track of current items player has)
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Destroy(gameObject);
+        }
+
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        // Modifies the ships sprite based on their health
+       if (currentHealth <= (maxHealth * 0.25))
+        {
+            animator.SetInteger("HealthState", (int)HealthState.Low);
+        }
+       else if (currentHealth <= (maxHealth * 0.5))
+        {
+            animator.SetInteger("HealthState", (int)HealthState.Half);
+        }
+        else if (currentHealth == maxHealth)
+        {
+            animator.SetInteger("HealthState", (int)HealthState.Full);
+        }
+
+        Debug.Log(currentHealth);
+
+    }
+
 }
