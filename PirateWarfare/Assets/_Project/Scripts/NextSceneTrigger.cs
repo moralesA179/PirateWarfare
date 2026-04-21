e9ce57d76270c5be349d7b5b3f0da077cddbd830
Edgar Quinones <edgarq1215@gmail.com>
2026-04-21 11:38:19 -0400

    fixed nav mesh, looking at stuff

--- PirateWarfare/Assets/_Project/Scripts/NextSceneTrigger.cs
+++ PirateWarfare/Assets/_Project/Scripts/NextSceneTrigger.cs
@@ -9,7 +9,7 @@ public class NextSceneTrigger : MonoBehaviour
     // Changes to a scene based on the variable set
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && PlayerData.levelsComplete >= (level - 1))
+        if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(sceneName);
         }
